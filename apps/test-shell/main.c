46d18f60928ed4ecd3e254a7b40c9d2c43a3bd64
J. David Ibáñez <jdavid.ibp@gmail.com>
2020-08-13 10:35:24 +0200

    test-shell: add vfs commands
    This mounts the SD card, so the "ls /" command works.

--- apps/test-shell/main.c
+++ apps/test-shell/main.c
@@ -8,6 +8,10 @@
 #include "shell.h"
 //#include "ztimer.h"
 
+#include "fs/fatfs.h"
+#include "mtd.h"
+#include "vfs.h"
+
 // WSN Sensors
 #include "lis3331ldh.h"
 #include "bmx280.h"
@@ -274,6 +278,53 @@ static int cmd_echo(int argc, char **argv) {
 }
 */
 
+
+
+/*
+ * Filesystem
+ */
+
+#include "board.h" /* MTD_0 should be defined in board.h */
+
+/* Configure MTD device for SD card if none is provided */
+#if !defined(MTD_0) && MODULE_MTD_SDCARD
+#include "mtd_sdcard.h"
+#include "sdcard_spi.h"
+#include "sdcard_spi_params.h"
+
+#define SDCARD_SPI_NUM ARRAY_SIZE(sdcard_spi_params)
+
+/* SD card devices are provided by auto_init_sdcard_spi */
+extern sdcard_spi_t sdcard_spi_devs[SDCARD_SPI_NUM];
+
+/* Configure MTD device for the first SD card */
+static mtd_sdcard_t mtd_sdcard_dev = {
+    .base = {
+        .driver = &mtd_sdcard_driver
+    },
+    .sd_card = &sdcard_spi_devs[0],
+    .params = &sdcard_spi_params[0],
+};
+static mtd_dev_t *mtd0 = (mtd_dev_t*)&mtd_sdcard_dev;
+#define MTD_0 mtd0
+#endif
+
+
+mtd_dev_t *fatfs_mtd_devs[FF_VOLUMES];
+
+static fatfs_desc_t fs_desc; // By default .vol_idx = 0
+static vfs_mount_t mountp = {
+    .fs = &fatfs_file_system,       // Driver
+    .private_data = &fs_desc,       // Driver data
+    .mount_point = "/",             // Mount point
+};
+
+
+
+/*
+ * Main
+ */
+
 const shell_command_t shell_commands[] = {
     {"acc", "accelerometer", cmd_acc},
     {"bme", "read BME_280", cmd_bme},
@@ -290,6 +341,15 @@ int main(void)
     // Print some info
     puts("app: test-shell");
     printf("Board=%s MCU=%s\n", RIOT_BOARD, RIOT_MCU);
+
+    // Mount the SD card
+    fatfs_mtd_devs[fs_desc.vol_idx] = MTD_0;
+    int error = vfs_mount(&mountp);
+    if (error) {
+        printf("Error mounting SD card\n");
+    } else {
+        printf("SD card mounted\n");
+    }
 
     // Run the shell
     char buffer[SHELL_DEFAULT_BUFSIZE]; // 128
