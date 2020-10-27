a0573448394105b2e0a1977453444697a4830b8e
J. David Ibáñez <jdavid.ibp@gmail.com>
2020-10-27 10:50:21 +0100

    arduino library works
    Have to use print_float (from fmt) because RIOT's Serial.print doesn't
    work with floats.

--- apps/test-arduino/main.cpp
+++ apps/test-arduino/main.cpp
@@ -1,39 +1,46 @@
-#include "arduino_board.h"
+#include <fmt.h>
 
+#include <arduino_board.h>
 #include <Wire.h>
 
 #include <SparkFunBME280.h>
 
-#define LIS3DH_CS 10
 
-BME280 mySensorA; //Uses default I2C address 0x77
+BME280 mySensor;
 
 int main(void)
 {
     Serial.begin(9600);
-    Serial.println("Example showing alternate I2C addresses");
+    Serial.println("Read samples from Sparkfun's BME280 at 0x77 I2C address");
 
     Wire.begin();
 
-    mySensorA.setI2CAddress(0x77); //The default for the SparkFun Environmental Combo board is 0x77 (jumper open).
-    //If you close the jumper it is 0x76
-    //The I2C address must be set before .begin() otherwise the cal values will fail to load.
+    // The default for the SparkFun Environmental Combo board is 0x77 (jumper open).
+    // If you close the jumper it is 0x76
+    // The I2C address must be set before .begin() otherwise the cal values will fail to load.
+    mySensor.setI2CAddress(0x77);
 
-    if(mySensorA.beginI2C() == false) Serial.println("Sensor A connect failed");
+    if (mySensor.beginI2C() == false) {
+        Serial.println("Sensor 0x77 connect failed");
+        return -1;
+    }
 
     while (1) {
         Serial.print("Humidity: ");
-        Serial.print(mySensorA.readFloatHumidity(), 0);
+        print_float(mySensor.readFloatHumidity(), 0);
+        //Serial.print(mySensor.readFloatHumidity(), 0);
 
         Serial.print(" Pressure: ");
-        Serial.print(mySensorA.readFloatPressure(), 0);
+        print_float(mySensor.readFloatPressure(), 0);
+        //Serial.print(mySensor.readFloatPressure(), 0);
 
         Serial.print(" Temp: ");
-        Serial.print(mySensorA.readTempC(), 2);
+        print_float(mySensor.readTempC(), 2);
+        //Serial.print(mySensor.readTempC(), 2);
 
         Serial.println();
 
-        delay(1000);
+        delay(3000);
     }
 
     return 0;
