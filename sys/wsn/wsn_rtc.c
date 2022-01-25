2e4a845b35dc267d017e183a06fc702873b3f5cd
J. David Ibáñez <jdavid.ibp@gmail.com>
2022-01-25 08:50:57 +0100

    Fix building without ds3231 module

--- sys/wsn/wsn_rtc.c
+++ sys/wsn/wsn_rtc.c
@@ -1,6 +1,8 @@
 // Riot
+#ifdef MODULE_DS3231
 #include <ds3231.h>
 #include <ds3231_params.h>
+#endif
 
 // Project
 #include <log.h>
@@ -12,20 +14,19 @@ static ds3231_t _dev;
 
 int wsn_rtc_init(void)
 {
-    // DS3231
-    if (IS_USED(MODULE_DS3231)) {
-        /* initialize the device */
-        ds3231_params_t params= ds3231_params[0];
-        params.opt     = DS3231_OPT_BAT_ENABLE;
-        params.opt    |= DS3231_OPT_INTER_ENABLE;
-        int res = ds3231_init(&_dev, &params);
-        if (res != 0) {
-            LOG_ERROR("unable to initialize DS3231 [I2C initialization error]");
-            return -1;
-        }
-
-        return 0;
+#ifdef MODULE_DS3231
+    /* initialize the device */
+    ds3231_params_t params= ds3231_params[0];
+    params.opt     = DS3231_OPT_BAT_ENABLE;
+    params.opt    |= DS3231_OPT_INTER_ENABLE;
+    int res = ds3231_init(&_dev, &params);
+    if (res != 0) {
+        LOG_ERROR("unable to initialize DS3231 [I2C initialization error]");
+        return -1;
     }
+
+    return 0;
+#endif
 
     // No RTC
     LOG_WARNING("Missing RTC module");
@@ -34,18 +35,18 @@ int wsn_rtc_init(void)
 
 
 int wsn_rtc_temp(int16_t *temp) {
-    // DS3231
-    if (IS_USED(MODULE_DS3231)) {
-        int res = ds3231_get_temp(&_dev, temp);
-        if (res != 0) {
-            LOG_ERROR("unable to read temperature");
-            return -1;
-        }
-
-        return 0;
+#ifdef MODULE_DS3231
+    int res = ds3231_get_temp(&_dev, temp);
+    if (res != 0) {
+        LOG_ERROR("unable to read temperature");
+        return -1;
     }
 
+    return 0;
+#endif
+
     // No RTC
+    (void)temp;
     LOG_WARNING("Missing RTC module");
     return -1;
 }
@@ -53,18 +54,18 @@ int wsn_rtc_temp(int16_t *temp) {
 
 int wsn_rtc_time_get(struct tm *time)
 {
-    // DS3231
-    if (IS_USED(MODULE_DS3231)) {
-        int res = ds3231_get_time(&_dev, time);
-        if (res != 0) {
-            LOG_ERROR("unable to read time");
-            return -1;
-        }
-
-        return 0;
+#ifdef MODULE_DS3231
+    int res = ds3231_get_time(&_dev, time);
+    if (res != 0) {
+        LOG_ERROR("unable to read time");
+        return -1;
     }
 
+    return 0;
+#endif
+
     // No RTC
+    (void)time;
     LOG_WARNING("Missing RTC module");
     return -1;
 }
@@ -72,18 +73,18 @@ int wsn_rtc_time_get(struct tm *time)
 
 int wsn_rtc_time_set(struct tm *time)
 {
-    // DS3231
-    if (IS_USED(MODULE_DS3231)) {
-        int res = ds3231_set_time(&_dev, time);
-        if (res != 0) {
-            LOG_ERROR("unable to set time");
-            return 1;
-        }
-
-        return 0;
+#ifdef MODULE_DS3231
+    int res = ds3231_set_time(&_dev, time);
+    if (res != 0) {
+        LOG_ERROR("unable to set time");
+        return 1;
     }
 
+    return 0;
+#endif
+
     // No RTC
+    (void)time;
     LOG_WARNING("Missing RTC module");
     return -1;
 }
